4593354103670c7d3840c506f2b2783e271a6b8e
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-12-05 12:30:31 -0400

    landing section image - headings updated

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -6,8 +6,6 @@ const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
@@ -15,6 +13,13 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+
+  <VStack>
+      <Avatar size="2xl" name='Pete' src='https://i.pravatar.cc/150?img=7' />
+      <Heading size='1xl' paddingBottom='10'>{greeting}</Heading>
+      <Heading size='2xl' >{bio1}</Heading>
+      <Heading size='2xl' >{bio2}</Heading>
+    </VStack>
 
   </FullScreenSection>
 );
